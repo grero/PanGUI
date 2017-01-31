be08ab9523d78862298739fb6b2e8b5264f813ca
roger <roger.herikstad@gmail.com>
2017-01-31 14:36:40 +0800

    Generalizes to plotting generic plotobjects

--- main.py
+++ main.py
@@ -9,13 +9,29 @@ from matplotlib.backends.backend_qt4agg import (
 Ui_MainWindow, QMainWindow = loadUiType("GUI2.ui")
 
 class Main(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, plotobject):
+        """
+
+        :type plotobject: object
+        """
         super(Main, self).__init__()
         self.setupUi(self)
         self.prevButton.clicked.connect(self.goprev)
         self.nextButton.clicked.connect(self.gonext)
         self.index = 0
+        self.plotobject = plotobject
         self.currentIndex.setText(str(self.index))
+        fig1 = Figure()
+        fig1.set_facecolor((0.92, 0.92, 0.92))
+        fig1.set_tight_layout(True)
+        ax1f1 = fig1.add_subplot(111)
+        ax1f1.spines["top"].set_visible(False)
+        ax1f1.spines["right"].set_visible(False)
+        ax1f1.xaxis.set_ticks_position("bottom")
+        ax1f1.yaxis.set_ticks_position("left")
+        self.addmpl(fig1)
+        plotobject.plot(self.index,ax=self.fig.axes[0])
+        self.setWindowTitle(plotobject.title)
 
 
     def addmpl(self, fig):
@@ -33,25 +49,21 @@ class Main(QMainWindow, Ui_MainWindow):
         self.actionPan.triggered.connect(self.toolbar.pan)
 
     def gonext(self):
-        x = np.random.random(10)
-        y = np.random.random(10)
         self.fig.axes[0].cla()
-        self.fig.axes[0].scatter(x,y)
+        self.index = min(self.plotobject.data.shape[0]-1, self.index+1)
+        self.plotobject.plot(self.index, ax=self.fig.axes[0])
         self.canvas.draw()
-        self.index = min(10, self.index+1)
         self.currentIndex.setText(str(self.index))
 
     def goprev(self):
-        x = np.random.random(10)
-        y = np.random.random(10)
         self.fig.axes[0].cla()
-        self.fig.axes[0].scatter(x,y)
-        self.canvas.draw()
         self.index = max(0, self.index-1)
+        self.plotobject.plot(self.index, ax=self.fig.axes[0])
+        self.canvas.draw()
         self.currentIndex.setText(str(self.index))
 
 
-def create_window(window_class):
+def create_window(window_class,args):
     """
     Create a new window based on `window_class`. This works whether called from IPython terminal or as a script
     Based on: http://cyrille.rossant.net/making-pyqt4-pyside-and-ipython-work-together/
@@ -64,25 +76,9 @@ def create_window(window_class):
         app = QtGui.QApplication(sys.argv)
         app_created = True
     app.references = set()
-    window = window_class()
+    window = window_class(args)
     app.references.add(window)
     window.show()
     if app_created:
         app.exec_()
     return window
-
-if __name__ == "__main__":
-    import numpy as np
-
-    fig1 = Figure()
-    fig1.set_facecolor((0.92, 0.92, 0.92))
-    fig1.set_tight_layout(True)
-    ax1f1 = fig1.add_subplot(111)
-    ax1f1.spines["top"].set_visible(False)
-    ax1f1.spines["right"].set_visible(False)
-    ax1f1.xaxis.set_ticks_position("bottom")
-    ax1f1.yaxis.set_ticks_position("left")
-
-    ax1f1.plot(np.random.rand(5))
-    window = create_window(Main)
-    window.addmpl(fig1)
