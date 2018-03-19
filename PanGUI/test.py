1eaae62c5f5436e84738dbf17420b493f3476ab1
roger <roger.herikstad@gmail.com>
2018-03-19 10:53:24 +0800

    Fixes test function

--- PanGUI/test.py
+++ PanGUI/test.py
@@ -1,13 +1,28 @@
 import PanGUI
 from pylab import gcf
 import numpy as np
+import scipy
+import scipy.io as mio
+import os
+
+
+class DPVObject():
+    def __init__(self):
+        self.data = []
+
 
 class PlotObject():
-    def __init__(self,data,title="Test windwow"):
+    def __init__(self, data, title="Test windwow", name="", ext="mat"):
         self.data = data
         self.title = title
 
-    def plot(self,i,fig=None):
+    def load(self):
+        fname = os.path.join(self.name, self.ext)
+        if os.path.isfile(fname):
+            if self.ext == "mat":
+                dd = mio.loadmat(fname, squeeze_me=True)
+
+    def plot(self, i, fig=None):
         if fig is None:
             fig = gcf()
         if len(fig.axes) < 1:
@@ -15,12 +30,11 @@ class PlotObject():
         else:
             ax = fig.axes[0]
             ax.clear()
-        ax.plot(self.data[i,:])
+        ax.plot(self.data[i, :])
         return ax
+
 
 def test():
     data = np.random.random((10, 1000))
     pp = PlotObject(data)
-    ppg = PanGUI.create_window(PanGUI.Main, pp)
-
-
+    ppg = PanGUI.create_window(pp.plot, range(10))
