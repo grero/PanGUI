f7935fb1a768729248153bcf944f52a2673bcf96
roger <roger.herikstad@gmail.com>
2020-07-05 16:23:35 +0800

    Fixes an issue with setting the path for bool option

--- PanGUI/main.py
+++ PanGUI/main.py
@@ -110,7 +110,11 @@ class Main(QMainWindow, Ui_MainWindow):
                 action = QtWidgets.QAction(k, self)
                 action.setCheckable(True)
                 action.setChecked(v)
-                action.setData({"path": "_".join((cpath, menu.title()))})
+                if cpath:
+                    qpath = "_".join((cpath, menu.title()))
+                else:
+                    qpath = menu.title()
+                action.setData({"path": qpath})
                 menu.addAction(action)
             elif isinstance(v, dict):
                 if cpath != "":
