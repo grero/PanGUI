5172541015df944917a1a3829ac7ea486c9c295f
roger <roger.herikstad@gmail.com>
2020-07-09 13:26:55 +0800

    Bumps version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="PanGUI",
-      version="0.7.1",
+      version="0.8.0",
       description="Utility to pan through plots",
       url="https://github.com/grero/PanGUI.git",
       author="Roger Herikstad",
